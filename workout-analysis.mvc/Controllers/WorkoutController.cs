ed033fa32f00e53aa5d0b62bea2334b6dab552ef
Josh Rolstad <jrolstad@gmail.com>
2014-09-05 07:49:20 -0700

    Adding workout data list

---
+++ workout-analysis.mvc/Controllers/WorkoutController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mapmyfitnessapi_sdk.services;
+using mapmyfitnessapi_sdk.workouts;
+using workout_analysis.mvc.Models;
+
+namespace workout_analysis.mvc.Controllers
+{
+    public class WorkoutController : Controller
+    {
+        // GET: Workout
+        public ActionResult Index(WorkoutSearchRequest searchParameters)
+        {
+            var client = new WorkoutClient();
+
+            var request = new WorkoutApiRequest();
+            request.WithUserId(502434);
+
+            if (searchParameters != null && !string.IsNullOrWhiteSpace(searchParameters.StartedAfter))
+            {
+                request.WithStartedAfter(DateTime.Parse(searchParameters.StartedAfter));
+            }
+
+            if (searchParameters != null && !string.IsNullOrWhiteSpace(searchParameters.ApiKey))
+            {
+                request.WithApiKey(searchParameters.ApiKey);
+            }
+
+            if (searchParameters != null && !string.IsNullOrWhiteSpace(searchParameters.AccessToken))
+            {
+                request.WithAccessToken(searchParameters.AccessToken);
+            }
+
+            request
+                .WithApiKey("z8w3jv4y9b2nhq5qjgzb9pmpt4pwg9mc")
+                .WithAccessToken("0834ab313a742d2e5028a6b28351fa0673d852f5")
+                ;
+            var workouts = client.Get(request);
+            var sortedWorkouts = workouts.OrderByDescending(w => w.UpdatedDateTime);
+
+            return View(sortedWorkouts);
+        }
+    }
+}
---
+++ workout-analysis.mvc/Models/WorkoutSearchRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace workout_analysis.mvc.Models
+{
+    public class WorkoutSearchRequest
+    {
+        public string StartedAfter { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public string AccessToken { get; set; }
+    }
+}
