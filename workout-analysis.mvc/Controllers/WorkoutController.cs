920308b744cc5fe92225be67e651921b8243ed11
Josh Rolstad <jrolstad@gmail.com>
2014-09-05 15:02:39 -0700

    Adding defaults

--- workout-analysis.mvc/Controllers/WorkoutController.cs
+++ workout-analysis.mvc/Controllers/WorkoutController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using mapmyfitnessapi_sdk.models;
 using mapmyfitnessapi_sdk.services;
+using mapmyfitnessapi_sdk.users;
 using mapmyfitnessapi_sdk.workouts;
 using workout_analysis.mvc.Models;
 
@@ -17,8 +18,54 @@ namespace workout_analysis.mvc.Controllers
         {
             var client = new WorkoutClient();
 
+            SetRequestDefaults(requestingModel);
+
+            var request = CreateMapMyFitnessRequest(requestingModel);
+
+            var workouts = new List<Workout>();
+            if (!string.IsNullOrWhiteSpace(requestingModel.ApiKey) 
+                && !string.IsNullOrWhiteSpace(requestingModel.AccessToken) 
+                && !string.IsNullOrWhiteSpace(requestingModel.SearchRequest.UserId))
+            {
+                workouts = client.Get(request);
+            }
+
+            var indexViewModel = new WorkoutIndexViewModel
+            {
+                AccessToken = requestingModel.AccessToken,
+                ApiKey = requestingModel.ApiKey,
+                SearchRequest = requestingModel.SearchRequest ?? new WorkoutSearchRequest(),
+                Workouts = workouts.OrderByDescending(w => w.UpdatedDateTime).ToList()
+            };
+            return View(indexViewModel);
+        }
+
+        private static void SetRequestDefaults(WorkoutIndexViewModel requestingModel)
+        {
             requestingModel.SearchRequest = requestingModel.SearchRequest ?? new WorkoutSearchRequest();
 
+            if (string.IsNullOrWhiteSpace(requestingModel.SearchRequest.StartedAfter))
+                requestingModel.SearchRequest.StartedAfter = DateTime.Now.AddMonths(-1).ToShortDateString();
+
+            if (!string.IsNullOrWhiteSpace(requestingModel.AccessToken) &&
+                !string.IsNullOrWhiteSpace(requestingModel.ApiKey)
+                && string.IsNullOrWhiteSpace(requestingModel.SearchRequest.UserId))
+            {
+                var userClient = new UserClient();
+
+                var request = new UserApiRequest();
+                request.WithApiKey(requestingModel.ApiKey);
+                request.WithAccessToken(requestingModel.AccessToken);
+
+                var result = userClient.GetAuthenticatedUser(request);
+
+                requestingModel.SearchRequest.UserId = result.Id.ToString();
+            }
+
+        }
+
+        private static WorkoutApiRequest CreateMapMyFitnessRequest(WorkoutIndexViewModel requestingModel)
+        {
             var request = new WorkoutApiRequest();
 
             if (requestingModel.SearchRequest != null && !string.IsNullOrWhiteSpace(requestingModel.SearchRequest.StartedAfter))
@@ -40,23 +87,7 @@ namespace workout_analysis.mvc.Controllers
             {
                 request.WithAccessToken(requestingModel.AccessToken);
             }
-
-            var workouts = new List<Workout>();
-            if (!string.IsNullOrWhiteSpace(requestingModel.ApiKey) 
-                && !string.IsNullOrWhiteSpace(requestingModel.AccessToken) 
-                && !string.IsNullOrWhiteSpace(requestingModel.SearchRequest.UserId))
-            {
-                workouts = client.Get(request);
-            }
-
-            var indexViewModel = new WorkoutIndexViewModel
-            {
-                AccessToken = requestingModel.AccessToken,
-                ApiKey = requestingModel.ApiKey,
-                SearchRequest = requestingModel.SearchRequest ?? new WorkoutSearchRequest(),
-                Workouts = workouts.OrderByDescending(w => w.UpdatedDateTime).ToList()
-            };
-            return View(indexViewModel);
+            return request;
         }
     }
 }
