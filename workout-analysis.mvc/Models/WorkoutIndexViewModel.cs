b9486dc979440fcd3e91a8e769ff8a31fe4b0412
Josh Rolstad <jrolstad@gmail.com>
2015-12-11 08:35:12 -0800

    Adding distance in miles and activity type descriptions to make the data easier to work with

--- workout-analysis.mvc/Models/WorkoutIndexViewModel.cs
+++ workout-analysis.mvc/Models/WorkoutIndexViewModel.cs
@@ -13,4 +13,41 @@ namespace workout_analysis.mvc.Models
 
         public List<Workout> Workouts { get; set; } 
     }
+
+    public static class WorkoutExtensions
+    {
+        public static double? DistanceInMiles(this Workout workout)
+        {
+            const double metersPerMile = 1609.34;
+
+            if (!workout.Distance.HasValue) return null;
+
+            return workout.Distance/metersPerMile;
+        }
+
+        public static string ActivityTypeName(this Workout workout)
+        {
+            switch (workout.ActivityType)
+            {
+                case 172:
+                case 197:
+                case 16: 
+                case 136: 
+                case 187: 
+                    return "Run";
+                case 11:
+                    return "Bike";
+                case 75:
+                    return "Swim";
+                 case 24:
+                 case 510:
+                 case 109:
+                    return "Hike";
+                default:
+                    return workout.ActivityType.ToString();
+            }
+            return workout.ActivityType.ToString();
+        }
+
+    }
 }
